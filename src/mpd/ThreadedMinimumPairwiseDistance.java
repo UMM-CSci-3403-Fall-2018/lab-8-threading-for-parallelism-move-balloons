7174ac53954bd845e2fa8a72c18a029317719852
chong050 <chong050@morris.umn.edu>
2018-11-08 11:31:41 -0600

    Move class run method into respective inner-classes by range.

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -1,6 +1,6 @@
 package mpd;
 
-public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance, Runnable {
+public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance {
 
     public int[] values;
 
@@ -10,79 +10,6 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance,
         this.values = values;
 
         throw new UnsupportedOperationException();
-    }
-
-    public void run() {
-        // Example to base our run on from Serial
-        /*
-        long result = Integer.MAX_VALUE;
-        for (int i = 0; i < values.length; ++i) {
-            for (int j = 0; j < i; ++j) {
-                // Gives us all the pairs (i, j) where 0 <= j < i < values.length
-                long diff = Math.abs(values[i] - values[j]);
-                if (diff < result) {
-                    result = diff;
-                }
-            }
-        }
-        */
-
-        LowerLeft lowerLeft = new LowerLeft();
-        BottomRight bottomRight = new BottomRight();
-        TopRight topRight = new TopRight();
-        Center center = new Center();
-
-        int N = values.length;
-
-        long result = Integer.MAX_VALUE;
-        // Lower left DONE
-        if(lowerLeft.getRangeCheck()) {
-            for (int i = 0; i < N/2; ++i) {
-                for (int j = 0; j < i; ++j) {
-                    // Gives us all the pairs (i, j) where 0 ≤ j < i < N/2
-                    long diff = Math.abs(values[i] - values[j]);
-                    if (diff < result) {
-                        result = diff;
-                    }
-                }
-            }
-        }
-        // Bottom right DONE?
-        if(bottomRight.getRangeCheck()) {
-            for (int i = 0; i < N; ++i) {
-                for (int j = 0; j + N/2 < i; ++j) {
-                    // Gives us all the pairs (i, j) where N/2 ≤ j + N/2 < i < N
-                    long diff = Math.abs(values[i] - values[j]);
-                    if (diff < result) {
-                        result = diff;
-                    }
-                }
-            }
-        }
-        // Top right DONE? probs not
-        if(topRight.getRangeCheck()) {
-            for (int i = N/2; i < N; ++i) {
-                for (int j = N/2; j < i; ++j) {
-                    // Gives us all the pairs (i, j) where N/2 ≤ j < i < N
-                    long diff = Math.abs(values[i] - values[j]);
-                    if (diff < result) {
-                        result = diff;
-                    }
-                }
-            }
-        }
-        // Center DONE?
-        if(center.getRangeCheck()) {
-            for (int i = 0; i < N; ++i) {
-                for (int j = 0; i <= j + N/2; ++j) {
-                    // Gives us all the pairs (i, j) where N/2 ≤ i ≤ j + N/2 < N
-                    long diff = Math.abs(values[i] - values[j]);
-                    if (diff < result) {
-                        result = diff;
-                    }
-                }
-            }
-        }
     }
 
     public int[] updateGlobalResult(int localResult) {
@@ -95,52 +22,79 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance,
         return null;
     }
 
-    private class LowerLeft{
-        private boolean rangeCheck = false;
+    private class LowerLeft implements Runnable {
 
-        boolean getRangeCheck() {
-            int i, j;
-            return rangeCheck;
+        public void run() {
+            int N = values.length;
+            long result = Integer.MAX_VALUE;
+
+            for (int i = 0; i < N/2; ++i) {
+                for (int j = 0; j < i; ++j) {
+                    // Gives us all the pairs (i, j) where 0 ≤ j < i < N/2
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
         }
 
-        synchronized void setRangeCheck(boolean newRangeCheck) {
-            rangeCheck = newRangeCheck;
-        }
     }
 
-    private class BottomRight {
-        private boolean rangeCheck = false;
+    private class BottomRight implements Runnable {
 
-        boolean getRangeCheck() {
-            return rangeCheck;
+        public void run() {
+            int N = values.length;
+            long result = Integer.MAX_VALUE;
+
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j + N/2 < i; ++j) {
+                    // Gives us all the pairs (i, j) where N/2 ≤ j + N/2 < i < N
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
         }
 
-        synchronized void setRangeCheck(boolean newRangeCheck) {
-            rangeCheck = newRangeCheck;
-        }
     }
 
-    private class TopRight {
-        private boolean rangeCheck = false;
+    private class TopRight implements Runnable {
 
-        boolean getRangeCheck() {
-            return rangeCheck;
+        public void run() {
+            int N = values.length;
+            long result = Integer.MAX_VALUE;
+
+            for (int i = N/2; i < N; ++i) {
+                for (int j = N/2; j < i; ++j) {
+                    // Gives us all the pairs (i, j) where N/2 ≤ j < i < N
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
         }
 
-        synchronized void setRangeCheck(boolean newRangeCheck) {
-            rangeCheck = newRangeCheck;
-        }
     }
 
-    private class Center {
-        private boolean rangeCheck = false;
+    private class Center implements Runnable {
 
-        boolean getRangeCheck() {
-            return rangeCheck;
+        public void run() {
+            int N = values.length;
+            long result = Integer.MAX_VALUE;
+
+            for (int j = 0; j < N; ++j) {
+                for (int i = 0; i < N; ++i) {
+                    // Gives us all the pairs (i, j) where N/2 ≤ i ≤ j + N/2 < N
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
         }
 
-        synchronized void setRangeCheck(boolean newRangeCheck) {
-            rangeCheck = newRangeCheck;
-        }
     }
 }
