7cbe879a8f225bdcbc011194cfcb93ccb7b338d9
chong050 <chong050@morris.umn.edu>
2018-11-08 11:11:27 -0600

    Create range logic. May or may not work.

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -2,10 +2,12 @@ package mpd;
 
 public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance, Runnable {
 
+    public int[] values;
+
     @Override
     public long minimumPairwiseDistance(int[] values) {
-        //public int[] valArray = values;
 
+        this.values = values;
 
         throw new UnsupportedOperationException();
     }
@@ -25,7 +27,62 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance,
         }
         */
 
+        LowerLeft lowerLeft = new LowerLeft();
+        BottomRight bottomRight = new BottomRight();
+        TopRight topRight = new TopRight();
+        Center center = new Center();
 
+        int N = values.length;
+
+        long result = Integer.MAX_VALUE;
+        // Lower left DONE
+        if(lowerLeft.getRangeCheck()) {
+            for (int i = 0; i < N/2; ++i) {
+                for (int j = 0; j < i; ++j) {
+                    // Gives us all the pairs (i, j) where 0 ≤ j < i < N/2
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+        }
+        // Bottom right DONE?
+        if(bottomRight.getRangeCheck()) {
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j + N/2 < i; ++j) {
+                    // Gives us all the pairs (i, j) where N/2 ≤ j + N/2 < i < N
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+        }
+        // Top right DONE? probs not
+        if(topRight.getRangeCheck()) {
+            for (int i = N/2; i < N; ++i) {
+                for (int j = N/2; j < i; ++j) {
+                    // Gives us all the pairs (i, j) where N/2 ≤ j < i < N
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+        }
+        // Center DONE?
+        if(center.getRangeCheck()) {
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; i <= j + N/2; ++j) {
+                    // Gives us all the pairs (i, j) where N/2 ≤ i ≤ j + N/2 < N
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+        }
     }
 
     public int[] updateGlobalResult(int localResult) {
@@ -38,7 +95,20 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance,
         return null;
     }
 
-    private class lowerLeft{
+    private class LowerLeft{
+        private boolean rangeCheck = false;
+
+        boolean getRangeCheck() {
+            int i, j;
+            return rangeCheck;
+        }
+
+        synchronized void setRangeCheck(boolean newRangeCheck) {
+            rangeCheck = newRangeCheck;
+        }
+    }
+
+    private class BottomRight {
         private boolean rangeCheck = false;
 
         boolean getRangeCheck() {
@@ -50,7 +120,7 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance,
         }
     }
 
-    private class bottomRight {
+    private class TopRight {
         private boolean rangeCheck = false;
 
         boolean getRangeCheck() {
@@ -62,19 +132,7 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance,
         }
     }
 
-    private class topRight {
-        private boolean rangeCheck = false;
-
-        boolean getRangeCheck() {
-            return rangeCheck;
-        }
-
-        synchronized void setRangeCheck(boolean newRangeCheck) {
-            rangeCheck = newRangeCheck;
-        }
-    }
-
-    private class center {
+    private class Center {
         private boolean rangeCheck = false;
 
         boolean getRangeCheck() {
